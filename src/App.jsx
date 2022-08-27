9378b00c1cd7142dc64ff078c877ec0206f118e2
Simon Levey <lev_47@hotmail.com>
2022-08-27 23:44:57 +0800

    added light/dark mode compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { useContext } from "react";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Intro from "./components/intro/Intro";
 import ProjectList from "./components/projectList/ProjectList";
 import Toggle from "./components/toggle/Toggle";
+import { ThemeContext } from "./context";
 
 const App = () => {
+
+  const theme = useContext(ThemeContext)
+  const darkMode = theme.state.darkMode;
+
   return (
-  <div>
+  <div style={{backgroundColor: darkMode ? "#222" : "white", color: darkMode && "white"}}>
     <Toggle/>
     <Intro/>
     <About/>
